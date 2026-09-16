997cd4d5278d0c968aafd913f47c17df61654967
Hugo <h.claveille@gmail.com>
2026-09-16 12:00:43 +0200

    push avant d'aller manger

--- PB1/localSearch.py
+++ PB1/localSearch.py
@@ -52,31 +52,34 @@ def local_search(N_vid, N_endpoint, N_requests, N_caches, caches_sizes, videoSiz
         nbVideos = N_vid
     if iteration == 0:
         return caches, caches_sizes
-    print(f"Testing local search iteration {iteration} with {nbCaches}/{N_caches} caches and {nbVideos}/{N_vid} videos")
+    # print(f"Testing local search iteration {iteration} with {nbCaches}/{N_caches} caches and {nbVideos}/{N_vid} videos")
     # base_cost = compute_cost(caches, endpointData, requests)
     neighbors = []  # (cost, caches, caches_sizes, move)
 
     # ajouts
     for cache_id in range(nbCaches):
         cache = caches[cache_id]
-        for videoId in range(nbVideos):
+        for video in range(nbVideos):
             # print(f"Checking video {videoId} for cache {cache_id}")
-            if videoId in cache:
+            if video in cache:
                 continue
-            if videoSizes[videoId] <= caches_sizes[caches.index(cache)]:
-                caches[cache_id].append(videoId)
+            if videoSizes[video] <= caches_sizes[caches.index(cache)]:
+                caches[cache_id].append(video)
                 cost = compute_cost(caches, endpointData, requests)
-                caches[cache_id].remove(videoId)
-                neighbors.append((videoId, cost, caches, caches_sizes, (caches.index(cache), videoId)))
+                caches[cache_id].remove(video)
+                move = (1, cache_id, video) # 1 for addition, 0 for removal
+                neighbors.append((video, cost, caches, caches_sizes, move))
 
-    #suppression
-    for cache_id in range(nbCaches):
-        cache = caches[cache_id]
-        for videoId in cache:
-            caches[cache_id].remove(videoId)
-            cost = compute_cost(caches, endpointData, requests)
-            caches[cache_id].append(videoId)
-            neighbors.append((videoId, cost, caches, caches_sizes, (caches.index(cache), videoId)))
+    # suppression
+    if True:  # Disable removal for now
+        for cache_id in range(nbCaches):
+            cache = caches[cache_id]
+            for video in cache[:nbVideos]:  # Only consider the first nbVideos videos in the cache for removal
+                caches[cache_id].remove(video)
+                cost = compute_cost(caches, endpointData, requests)
+                caches[cache_id].append(video)
+                move = (0, cache_id, video) # 1 for addition, 0 for removal
+                neighbors.append((video, cost, caches, caches_sizes, move))
 
     neighbors.sort(key=lambda x: x[1])
     if neighbors is None or len(neighbors) == 0:
@@ -85,7 +88,13 @@ def local_search(N_vid, N_endpoint, N_requests, N_caches, caches_sizes, videoSiz
     best_neighbor = neighbors[0] if neighbors else (float('inf'), caches, caches_sizes, None)
     # print(f"Best neighbor: {best_neighbor} with cost {best_neighbor[1]}")
     #on applique le mouvement du meilleur voisin
-    caches[best_neighbor[4][0]].append(best_neighbor[4][1])
+    if best_neighbor[4][0] == 1:
+        caches[best_neighbor[4][1]].append(best_neighbor[4][2])
+    else:
+        print(f"Supression")
+        caches[best_neighbor[4][1]].remove(best_neighbor[4][2])
+    # caches[best_neighbor[4][1]].append(best_neighbor[4][2])
+
     caches_sizes[best_neighbor[4][0]] -= videoSizes[best_neighbor[4][1]]
     
 
@@ -193,11 +202,11 @@ def main(args):
     video_sizes = video_sizes_sorted
     print(f"Preprocessing time: {time.time() - current_time:.4f} seconds")
     current_time = time.time()
-    local_search(N_vid, N_endpoint, N_request, N_cache, caches_sizes, video_sizes, caches, endpoints, requests, iteration=10, previous_moves=None, nbCaches=5, nbVideos=10)
+    local_search(N_vid, N_endpoint, N_request, N_cache, caches_sizes, video_sizes, caches, endpoints, requests, iteration=20, previous_moves=None, nbCaches=50, nbVideos=100)
     print(f"Local search time: {time.time() - current_time:.4f} seconds")
 
-    print("Final caches content:")
     if sum([len(cache) for cache in caches]) < 20:  
+        print("Final caches content:")
         for i, cache in enumerate(caches):
             print(f"Cache {i}: {cache}")
 
